56019d302cf5966e332a8f5f10ee55db7e36bd08
jenny.escobar <jenny.escobar@pragma.com.co>
2021-11-29 13:21:02 -0500

    Initial Commit

---
+++ manager/managerApp.swift
@@ -0,0 +1,20 @@
+//
+//  managerApp.swift
+//  manager
+//
+//  Created by Jenny escobar on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct managerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
